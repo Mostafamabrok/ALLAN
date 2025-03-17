3fdebb5a17655863990b526a82c09dee025e8f64
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2025-03-17 10:53:43 +0300

    minor non-effective changes

--- ALLAN/api_handling.py
+++ ALLAN/api_handling.py
@@ -1,2 +1,11 @@
+import OpenAI
+import os
+import shutil
+
 #This file will contain the functions for doing requests with the OpenAI API.
-#This removes excess clutter from other files and keeps things easy to work on in the future. 
+#This removes excess clutter from other files and keeps things easy to work on in the future.
+
+def send_message(sent_message):
+    return recived_message
+
+
