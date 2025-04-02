898d9d86aabf409fbfe59816a0a7c8783557045c
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2025-04-02 12:57:32 +0300

    Added Terminal UI

--- ALLAN/main.py
+++ ALLAN/main.py
@@ -3,7 +3,39 @@ import shutil
 
 from allan_core import *
 
+config = { #This should be editable by the user without going into the code later on. TBA
+    'model'     : 'gemma3:12b',
+    'permanence': True
+}
 
-x = Conversation(True, "gemma3:12b")
 
-x.terminal_chat()
+def allan_terminal_ui():
+
+    allan = Conversation(config["permanence"], config["model"])
+
+    version_number = "0.0.1"
+    
+    print("\n\n\nALLAN v" + version_number)
+
+    print("Choose an action from the prompts by typing its number and pressing enter.")
+    print("1-Chat with ALLAN in the Terminal")
+    print("2-Chat with ALLAN using Voice")
+    print("3-Restart Conversation")
+    print("4-Load old conversation")
+    print("5-Exit")
+
+    desired_action = input(": ")
+
+    if   desired_action == "1": allan.terminal_chat()
+    elif desired_action == "2": allan.voice_chat()
+    elif desired_action == "3": allan = Conversation(config["permanence"], config["model"]); print("Conversation Restarted.")
+    elif desired_action == "4": print("Feature is still in development. Check the Github Page for new updates.")
+    elif desired_action == "5": exit()
+    else: print("Please enter a valid input.\n")
+
+    allan_terminal_ui()
+    #Is there a better way to do this than recursiveness?
+    #I have no idea.
+    #This works for now though.
+
+allan_terminal_ui()
