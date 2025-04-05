fa6fc8bf7a022dee33f05c3c2f88065a9e1ed0e4
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2025-04-05 10:33:27 +0300

    added basic function detection feature.

--- ALLAN/allan_core.py
+++ ALLAN/allan_core.py
@@ -8,7 +8,7 @@ class Conversation:
         self.model = model
         self.system_prompt = """Your name is ALLAN and you are a helpful assistant.
                                 You have a variety of functions at your disposal.
-                                Here are your functions. You can call them by simply saying them like this: <[function_name]>
+                                Here are your functions. You can call them by simply saying them like this: <[function_name]>.
                                 <open_chrome>, <open_edge>, <press_key[key]>, <restart_conversation.>
                                 I have an automated tool set up that reads what you says and executes functions that you say like the ones above, but you must say them in that exact way.
                                 I CAN NOT call these functions. YOU call the functions by saying them (ie. "Of course I can open Chrome! <open_chrome>"). YOU are my assistant."""
@@ -22,6 +22,9 @@ class Conversation:
             self.messages = self.history
 
     def send(self, message):
+
+        import re
+
         self.messages.append({"role": "user", "content": message})
 
         if self.api_type == "ollama":
@@ -35,6 +38,12 @@ class Conversation:
             self.latest_response = api_response["choices"][0]["message"]
         
         self.messages.append({"role": "assistant", "content": self.latest_response})
+        
+        self.called_functions = []
+
+        response_funcs = re.findall(r"<[^<>]+>", self.latest_response)
+        for func in response_funcs:
+            self.called_functions.append(func)
 
     def terminal_chat(self):
         while True:
@@ -46,6 +55,7 @@ class Conversation:
 
             self.send(user_input)
             print(self.model + ": " + self.latest_response)
+            #DEBUG CODE: print("CALLED FUNCITIONS: " + str(self.called_functions))
     
 
     def voice_message(self):
